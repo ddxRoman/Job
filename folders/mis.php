d0c359beaec25794fc0c39021b0252748e7d5b33
ddxRoman <ddxman@mail.ru>
2023-02-09 14:33:23 +0300

    Ссылки подправил

--- folders/mis.php
+++ folders/mis.php
@@ -11,9 +11,9 @@
 </head>
 <body class="iframe-body">
     <div class="folders">
-<a href="https://client1.lab.helsi.pro" target="_blank"><button>КНЛОШВД</button></a>
+<a href="http://medcloud.csd.com.ua/" target="_blank"><button>CSD</button></a>
 
-<a href="https://lis.knpumbl.medcloud.pro/" target="_blank"><button>KNPUMBL</button></a>
+<a href="https://knpumbl.lab.helsi.pro/" target="_blank"><button>KNPUMBL</button></a>
 <a href="http://crm.newdiagnostics.ua/" target="_blank"><button>ND</button></a>
             <a href="https://miskbp.medcloud.pro/" target="_blank"><button>Medlab</button></a> 
             <a href="https://medcloud.astra-dia.ua/" target="_blank"><button>ASTRA</button></a>
@@ -24,7 +24,6 @@
             <a href="https://test.lab.helsi.pro/" target="_blank"><button>Тест ХОТфиксов</button></a>
             <a href="https://studylabs.helsi.pro" target="_blank"><button>СтадилабС</button></a>
             <a href="https://test.lab.helsi.pro/" target="_blank"><button>ХелсиТест</button></a>
-           
             </div>
 <!--<a href="" target="_blank"><button></button></a>
 <a href="" target="_blank"><button></button></a>
