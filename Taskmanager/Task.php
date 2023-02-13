1dc72f3679bfb18a51b6d8c370d73a5239d6cd51
ddxRoman <ddxman@mail.ru>
2023-02-13 21:54:53 +0300

    1. Стилизовал кое как коменті 2. В тесте рефактор Таскменеджера

--- Taskmanager/Task.php
+++ Taskmanager/Task.php
@@ -22,7 +22,6 @@
         $product = mysqli_fetch_all($product); // Выбирает все строки из набора $product и помещает их в массив  $product
         $comment = mysqli_query($connect, "SELECT * FROM `comments` ORDER BY `id` ASC "); // Подключение к определенной таблице, и получение Статуса записи
         $comment = mysqli_fetch_all($comment); // Выбирает все строки из набора $Comment и помещает их в массив  $Comments
-        $k = 0;
         foreach ($product as $products) { // Перебор массива $product c его записью в массив $productS
             $k++; ?>
             <script src="../JavaScript/accordion.js"></script> <!-- Cкрипт аккордиона-->
@@ -30,7 +29,7 @@
                 <div class="accordion__item">
                     <!-------------------------------------------- Проверяем если статус задачи 0, то выводим эти результаты----------------------------->
                     <? if ($products[3] == 0) { ?>
-                        <div class="accordion__header"> <!-- Верхний див где номер таски и имя -->
+                        <div class="accordion__header" style="background: linear-gradient(45deg, #c436369a, #d0d7d8, #d0d7d8, #d0d7d8, #c4363667)"> <!-- Верхний див где номер таски и имя -->
                         <a href="#" target="_blank"><p class="number"> № <?= $products[0]?>:</p></a><!-- Вот тут ссылка на весь экран-->
                             <p class="nametasks"><?= $products[1] ?></p>
                             <? if ($products[5] == 0) { // Проверка на статус таски, и вывод приоитета возле названия в заголовке
@@ -54,11 +53,9 @@
                                 <a href="../action/editTask.php?id=<?= $products[0] ?>"><img width="16px" height="16px" title="Редактировать" src="../file/icons/edit.png"></a> <!-- Кнопка редактировать -->
                                 <select name="priority" onchange="this.form.submit()"><!-- Селект с сортировкой Статусов задач, выглядит как хуйня, надо переделать что бы тут был запрос и с запроса шел этот статус-->
                                     <? if ($products[5] == 0) { ?>
-
                                         <option value="0">Backlog</option>
                                         <option value="1">Надо сделать</option>
                                         <option value="2">Нет знаний</option>
-
                                     <?
                                     } else if ($products[5] == 1) { ?>
                                         <option value="1">Надо сделать</option>
@@ -74,73 +71,61 @@
                                 </select>
                                 <form action="../action/accept_delete.php?id=<?= $products[0] ?>" method="post" name="real_delete">
                                     <a href="../action/accept_delete.php?id=<?= $products[0] ?>"><img src="/file/icons/delete.png" width="16px" height="16px"></a>
-
                                 </form>
-
                             </form>
                             <div class="accordion__content">
                                 <?= $products[2] ?>
                                 <a href="<?= $products[8]; ?>" target="_blank"><img class="pictures-in-tasks" src="<?= $products[8]; ?>"></a>
-
-
                             </div>
                             <a title="Профиль автора" href="/action/profile2.php?id=<?=$products[4];?>" target="_blank">
                                 <font class="owner"> <? echo $products[4]; ?> </font>
                             </a>
                             <font class="creation_date"><b>Создано:</b> <?= $products[6] ?></font>
 <!----------------------------------------Начало пати с комментариями------------------------------------------------------------------>
-
                             <div class="comments-block"><?
                                                         foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
                                                             if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
-                                                                echo ($comments[3] . " " . $comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
+                                                               ?><a title="Профиль автора" href="/action/profile2.php?id=<?=$comments[3];?>" target="_blank">
+                                <font class="owner-comment"> <? echo $comments[3]; ?> </font>
+                            </a><?
+                                                                echo ($comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
                                                             }
                                                         } ?>
+                                                        <div class="block-add-comments">
                                 <form action="../action/addComents.php" method="post" enctype="multipart/form-data">
                                     <textarea class="add-comments" name="contant"></textarea><br>
                                     <input type="file" name="picture"><br>
                                     <input type="hidden" name="id_task" value="<?= $products[0] ?>">
                                     <button>Добавить</button>
                                 </form>
+                                </div>
                             </div>
 <!----------------------------------------Конец пати с комментариями------------------------------------------------------------------>
                         <? } // Тут мы закрыли первую проверку, на то статус 0 Актуальный 
-
                     else  if ($products[3] == 1) {  ?>
                             <!---------------------------  Тут мы начали вторую проверку, на то статус 1 Закрыто---------------------------------->
                             <div style="background: linear-gradient(45deg, #58c436, #7ed66a, #b4e3ac, #e9ffe5);" class="accordion__header">
                                 <p class="number"> № <s> <?= $products[0]  ?> : </p>
                                 <p class="nametasks"><?= $products[1] ?></s></p>
-
-
                             </div>
                             <div class="accordion__body">
                                 <form action="../action/statusTask.php?id=<?= $products[0] ?>" method="post" name="form">
                                     <select name="currency" onchange="this.form.submit()">
-
                                         <option value="1">Выполнено</option>
                                         <option value="2">Не актуально</option>
                                         <option value="0">Актуально</option>
-
                                     </select>
-
                                 </form>
                                 <div color="yellow" class="accordion__content">
                                     <?= $products[2] ?>
                                     <img src="<?= $products[8]; ?>">
                                 </div>
-
-
                                 <a title="Профиль автора" href="/action/profile2.php?id=<?=$products[4];?>" target="_blank">
                                 <font class="owner"> <? echo $products[4]; ?> </font>
                             </a>
-
                                 <font class="creation_date"><b>Создано:</b> <?= $products[6] ?></font> <br>
                                 <font class="creation_date"><b>Закрыто:</b> <?= $products[7] ?></font>
-
-
 <!----------------------------------------Начало пати с комментариями------------------------------------------------------------------>
-
 <div class="comments-block"><?
                                                         foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
                                                             if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
@@ -155,10 +140,6 @@
                                 </form>
                             </div>
 <!----------------------------------------Конец пати с комментариями------------------------------------------------------------------>
-    
-
-                                
-
                             <?
                         }  // Тут мы закрыли вторую проверку, на то статус 1 Закрыто 
                         else if ($products[3] == 2) { ?>
@@ -166,16 +147,11 @@
                                 <div style="background: linear-gradient(45deg, #7a7a22, #bdba64, #e3e3ac, #ffffe5);" class="accordion__header">
                                     <p class="number"> № <s> <?= $products[0]  ?> : </p>
                                     <p class="nametasks"><?= $products[1] ?></s></p>
-
-
                                     <? if ($products[5] == 0) { // Проверка на статус таски, и вывод приоитета возле названия в заголовке
                                     ?><font class="prioritet-task0">Backlog</font><?
                                                                             } else if ($products[5] == 1) { ?><font class="prioritet-task1">Надо сделать</font> <?
                                                                                                         } else if ($products[5] == 2) { ?><font class="prioritet-task2">Нет знаний</font><?
-
-                                                                                                        }
-                                                                                                            ?>
-
+                                                                                                        }?>
                                 </div>
                                 <div class="accordion__body">
                                     <form action="../action/statusTask.php?id=<?= $products[0] ?>" method="post" name="form">
@@ -183,11 +159,8 @@
                                             <option value="2">Не актуально</option>
                                             <option value="1">Выполнено</option>
                                             <option value="0">Актуально</option>
-
                                         </select>
-
                                     </form>
-
                                     <div color="yellow" class="accordion__content">
                                         <?= $products[2] ?>
                                         <img src="<?= $products[8]; ?>">
@@ -195,11 +168,8 @@
                                     <a title="Профиль автора" href="/action/profile2.php?id=<?=$products[4];?>" target="_blank">
                                 <font class="owner"> <? echo $products[4]; ?> </font>
                             </a>
-
                                     <font class="creation_date"><b>Создано:</b> <?= $products[6] ?></font>
-
 <!----------------------------------------Начало пати с комментариями------------------------------------------------------------------>
-
 <div class="comments-block"><?
                                                         foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
                                                             if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
@@ -214,37 +184,24 @@
                                 </form>
                             </div>
 <!----------------------------------------Конец пати с комментариями------------------------------------------------------------------>
-    
-
-
-
                                 <? }
-
                                 ?>
-
-
                                 <script>
                                     new ItcAccordion(document.querySelector('.accordion'), {
                                         alwaysOpen: true
                                     });
                                 </script>
-
                                 </div>
                             </div>
                         <? }
-
                     if ($product[3] == 1) {
                         echo ('AAAa go home nigga');
                     } else ?>
-
                         </div>
-
-
                     <?
                 } else {
                     header('Location: ../action/NoRules.php');
                 }
                     ?>
 </body>
-
 </html>
--- Taskmanager/Test.php
+++ Taskmanager/Test.php
@@ -1,30 +1,148 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function comm()
-{
-            require_once '../action/connect.php'; // Проверка подключения к БД
-            $product = mysqli_query($connect, "SELECT * FROM `tasks` ORDER BY `status` ASC, `date_close` DESC "); // Подключение к определенной таблице, и получение Статуса записи
-            $product = mysqli_fetch_all($product); // Выбирает все строки из набора $product и помещает их в массив  $product
-            $comment = mysqli_query($connect, "SELECT * FROM `comments` ORDER BY `id` ASC "); // Подключение к определенной таблице, и получение Статуса записи
-            $comment = mysqli_fetch_all($comment); // Выбирает все строки из набора $Comment и помещает их в массив  $Comments
-            ?><div class="comments-block"><?
-    foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
-        if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
-            echo ($comments[3] . " " . $comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
-        }
-    } ?>
-<form action="../action/addComents.php" method="post" enctype="multipart/form-data">
-<textarea class="add-comments" name="contant"></textarea><br>
-<input type="file" name="picture"><br>
-<input type="hidden" name="id_task" value="<?= $products[0] ?>">
-<button>Добавить</button>
-</form>
-</div>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="../css/styleaccordion.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+    <?
+    require_once '../action/connect.php'; // Проверка подключения к БД
+    $product = mysqli_query($connect, "SELECT * FROM `tasks` ORDER BY `status` ASC, `date_close` DESC "); // Подключение к определенной таблице, и получение Статуса записи
+    $product = mysqli_fetch_all($product); // Выбирает все строки из набора $product и помещает их в массив  $product
+    $comment = mysqli_query($connect, "SELECT * FROM `comments` ORDER BY `id` ASC "); // Подключение к определенной таблице, и получение Статуса записи
+    $comment = mysqli_fetch_all($comment); // Выбирает все строки из набора $Comment и помещает их в массив  $Comments
+
+function comment($products, $comment){?>
+
+    <div class="comments-block"><?
+                                   foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
+                                        if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
+                                             echo ($comments[3] . " " . $comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
+                                              }
+                                    } ?>
+                                <form action="../action/addComents.php" method="post" enctype="multipart/form-data">
+                                    <textarea class="add-comments" name="contant"></textarea><br>
+                                    <input type="file" name="picture"><br>
+                                    <input type="hidden" name="id_task" value="<?= $products[0] ?>">
+                                    <button>Добавить</button>
+                                </form>
+                            </div>
+<!----------------------------------------Конец пати с комментариями------------------------------------------------------------------>
 <?
-return $comment. $comments. $product;
 }
 
-comm($A,$B,$C);
-echo $A, $B,$C;
+    function stepen($product, $status, $color, $prioritet)
+    {
+        foreach ($product as $products) { 
+    ?><script src="../JavaScript/accordion.js"></script> <!-- Cкрипт аккордиона-->
+            <div id="accordion" class="accordion" style="max-width: 30rem; margin: 1rem auto;"> <!-- общий див всего акордиона-->
+                <div class="accordion__item"><?
+                         if ($products[3] == $status) { ?>
+                             <div class="accordion__header" style="<?=$color;?>">
+                        <a href="#" target="_blank">
+                            <p class="number"> № <?= $products[0] ?>:</p>
+                        </a><!-- Вот тут ссылка на весь экран-->
+                        <p class="nametasks"><?= $products[1] ?></p>
+                        <?
+                        if ($products[5] == 0) { // Проверка на приоритет таски, и вывод приоитета возле названия в заголовке
+                        ?><font class="prioritet-task0">Backlog</font><?// Проверяем приоритет
+                            } else if ($products[5] == 1) { 
+                                ?><font class="prioritet-task1">Надо сделать</font> <?
+                            
+                                    } else if ($products[5] == 2) { 
+                                        ?><font class="prioritet-task2">Нет знаний</font>
+                             <?}?>
+                                                </div>
+                                                <div class="accordion__body">
+                            <form action="../action/statusTask.php?id=<?= $products[0] ?>" method="post" name="form"> <!-- форма с селектами-->
+                            <select name='currency' onchange='this.form.submit()'>
+                                     <!-- Проверяем если статус задачи 1 то выводим Селект где первая запись Активный и тд. -->
+<? if($products[3]==0){
+                                  $status_task="
+                                  <option value='0'>Актуально</option>
+                                    <option value='1'>Выполнено</option>
+                                    <option value='2'>Не актуально</option>
+                                    ";
+}else if($products[3]==1)  {
+    $status_task="
+      <option value='1'>Выполнено</option>
+      <option value='0'>Актуально</option>
+      <option value='2'>Не актуально</option>
+      ";
+}else if($products[3]==2){
+    $status_task="
+    <option value='2'>Не актуально</option>
+    <option value='0'>Актуально</option>
+    <option value='1'>Выполнено</option>
+    ";?></select><?
+}
+                                 echo $status_task ?>
+                               
+                                <a href="../action/editTask.php?id=<?= $products[0]?>"><img width="16px" height="16px" title="Редактировать" src="../file/icons/edit.png"></a> <!-- Кнопка редактировать -->
+                             <select name="priority" onchange="this.form.submit()"> <!--Селект с сортировкой Статусов задач, выглядит как хуйня, надо переделать что бы тут был запрос и с запроса шел этот статус -->
+                                <!-- Я же уже 1 раз проверил приоритет таски-->
+                                    <? if ($products[5] == 0) { 
+                                        $prioritet_task="
+                                        <option value='0'>Backlog</option>
+                                        <option value='1'>Надо сделать</option>
+                                        <option value='2'>Нет знаний</option>";
+                                    
+                                    } else if ($products[5] == 1) { 
+                                        $prioritet_task="
+                                        <option value='1'>Надо сделать</option>
+                                        <option value='0'>Backlog</option>
+                                        <option value='2'>Нет знаний</option>";
+                                    
+                                    } else if ($products[5] == 2) { 
+                                        $prioritet_task="
+                                        <option value='2'>Нет знаний</option>
+                                        <option value='0'>Backlog</option>
+                                        <option value='1'>Надо сделать</option>";
+                                        
+                                    } ?>
+                                    
+                                </select> 
+                                <select><? echo $prioritet_task?></select>
+                                <form action="../action/accept_delete.php?id=<?= $products[0] ?>" method="post" name="real_delete">
+                                    <a href="../action/accept_delete.php?id=<?= $products[0] ?>"><img src="/file/icons/delete.png" width="16px" height="16px"></a>
+                                </form>
+                            </form>
+                            <div class="accordion__content">
+                                <?= $products[2] ?> 
+                                <a href="<?= $products[8]; ?>" target="_blank"><img class="pictures-in-tasks" src="<?= $products[8]; ?>"></a>
 
-?>
+                            </div>
+                            <a title="Профиль автора" href="/action/profile2.php?id=<?=$products[4];?>" target="_blank">
+                                <font class="owner"> <? echo $products[4]; ?> </font>
+                            </a>
+                            <font class="creation_date"><b>Создано:</b> <?= $products[6] ?></font>
+                            <? 
+                                 comment(3, 5);
+}?>
+<script>
+                                    new ItcAccordion(document.querySelector('.accordion'), {
+                                        alwaysOpen: true
+                                    });
+                                </script>
+                </div>
+            </div><?
+            // echo"Color- ". $color;
+                }
+            }
+   $color1="background: linear-gradient(45deg, #c436369a, #d0d7d8, #d0d7d8, #d0d7d8, #c4363667)";
+   $color2="background: linear-gradient(45deg, #58c436, #7ed66a, #b4e3ac, #e9ffe5)";
+   $color3="background: linear-gradient(45deg, #7a7a22, #bdba64, #e3e3ac, #ffffe5)";
+            stepen($product, 0, $color1,0);
+            stepen($product, 1, $color2,1);
+            stepen($product, 2, $color3,2);
+
+                    ?>
+</body>
+</html>
