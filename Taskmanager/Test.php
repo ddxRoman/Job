f8077fd468fe4e9ad9745ce648687fdec729015e
ddxRoman <ddxman@mail.ru>
2023-02-16 11:58:05 +0300

    Задатки на Туду лист

--- Taskmanager/Test.php
+++ Taskmanager/Test.php
@@ -24,7 +24,7 @@ function comment($products, $comment){?>
     <div class="comments-block"><?
                                    foreach ($comment as $comments) { // Перебор массива $ c его записью в массив $
                                         if ($comments[1] == $products[0]) {//Проверяем если айди таска комента равно айди самого таска то выводим его
-                                             echo ($comments[3] . " " . $comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
+                                             echo ($comments[3] . "66666666666666666 " . $comments[4] . "<br><hr>" . $comments[2]  . "<a href='$comments[5]'><img src='$comments[5]' class='pictures-in-tasks'></a> <hr class='end-comments'>");
                                               }
                                     } ?>
                                 <form action="../action/addComents.php" method="post" enctype="multipart/form-data">
@@ -64,52 +64,51 @@ function comment($products, $comment){?>
                             <form action="../action/statusTask.php?id=<?= $products[0] ?>" method="post" name="form"> <!-- форма с селектами-->
                             <select name='currency' onchange='this.form.submit()'>
                                      <!-- Проверяем если статус задачи 1 то выводим Селект где первая запись Активный и тд. -->
-<? if($products[3]==0){
-                                  $status_task="
+<? if($products[3]==0){?>
+                                 
                                   <option value='0'>Актуально</option>
                                     <option value='1'>Выполнено</option>
-                                    <option value='2'>Не актуально</option>
-                                    ";
+                                    <option value='2'>Не актуально</option><?
+                                    
 }else if($products[3]==1)  {
-    $status_task="
+?>
       <option value='1'>Выполнено</option>
       <option value='0'>Актуально</option>
       <option value='2'>Не актуально</option>
-      ";
-}else if($products[3]==2){
-    $status_task="
+      
+<?}else if($products[3]==2){?>
+
     <option value='2'>Не актуально</option>
     <option value='0'>Актуально</option>
     <option value='1'>Выполнено</option>
-    ";?></select><?
+    </select><?
 }
-                                 echo $status_task ?>
+                                  ?>
                                
-                                <a href="../action/editTask.php?id=<?= $products[0]?>"><img width="16px" height="16px" title="Редактировать" src="../file/icons/edit.png"></a> <!-- Кнопка редактировать -->
-                             <select name="priority" onchange="this.form.submit()"> <!--Селект с сортировкой Статусов задач, выглядит как хуйня, надо переделать что бы тут был запрос и с запроса шел этот статус -->
+                               
+                              <!--Селект с сортировкой Статусов задач, выглядит как хуйня, надо переделать что бы тут был запрос и с запроса шел этот статус -->
                                 <!-- Я же уже 1 раз проверил приоритет таски-->
-                                    <? if ($products[5] == 0) { 
-                                        $prioritet_task="
+                                <select name="priority" onchange="this.form.submit()">
+                                <a href="../action/editTask.php?id=<?= $products[0]?>"><img width="16px" height="16px" title="Редактировать" src="../file/icons/edit.png"></a> <!-- Кнопка редактировать -->
+                                    <? if ($products[5] == 0) { ?>
+                                        <select> 
                                         <option value='0'>Backlog</option>
                                         <option value='1'>Надо сделать</option>
-                                        <option value='2'>Нет знаний</option>";
-                                    
+                                        <option value='2'>Нет знаний</option></select> 
+                                    <?
                                     } else if ($products[5] == 1) { 
-                                        $prioritet_task="
+                                      ?>  <select> 
                                         <option value='1'>Надо сделать</option>
                                         <option value='0'>Backlog</option>
-                                        <option value='2'>Нет знаний</option>";
-                                    
+                                        <option value='2'>Нет знаний</option></select> 
+                                    <?
                                     } else if ($products[5] == 2) { 
-                                        $prioritet_task="
+                                      ?>  <select> 
                                         <option value='2'>Нет знаний</option>
                                         <option value='0'>Backlog</option>
-                                        <option value='1'>Надо сделать</option>";
-                                        
-                                    } ?>
-                                    
-                                </select> 
-                                <select><? echo $prioritet_task?></select>
+                                        <option value='1'>Надо сделать</option>
+                                        </select> 
+                                   <? }?>
                                 <form action="../action/accept_delete.php?id=<?= $products[0] ?>" method="post" name="real_delete">
                                     <a href="../action/accept_delete.php?id=<?= $products[0] ?>"><img src="/file/icons/delete.png" width="16px" height="16px"></a>
                                 </form>
@@ -117,14 +116,13 @@ function comment($products, $comment){?>
                             <div class="accordion__content">
                                 <?= $products[2] ?> 
                                 <a href="<?= $products[8]; ?>" target="_blank"><img class="pictures-in-tasks" src="<?= $products[8]; ?>"></a>
-
                             </div>
                             <a title="Профиль автора" href="/action/profile2.php?id=<?=$products[4];?>" target="_blank">
                                 <font class="owner"> <? echo $products[4]; ?> </font>
                             </a>
                             <font class="creation_date"><b>Создано:</b> <?= $products[6] ?></font>
                             <? 
-                                 comment(3, 5);
+                                 comment($products, $comment);
 }?>
 <script>
                                     new ItcAccordion(document.querySelector('.accordion'), {
@@ -133,7 +131,7 @@ function comment($products, $comment){?>
                                 </script>
                 </div>
             </div><?
-            // echo"Color- ". $color;
+
                 }
             }
    $color1="background: linear-gradient(45deg, #c436369a, #d0d7d8, #d0d7d8, #d0d7d8, #c4363667)";
@@ -142,7 +140,6 @@ function comment($products, $comment){?>
             stepen($product, 0, $color1,0);
             stepen($product, 1, $color2,1);
             stepen($product, 2, $color3,2);
-
                     ?>
 </body>
 </html>
